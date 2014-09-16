7f55048a5234cecbae61fec71844c42ace3b7fc4
Will Drexler <william.drexler@vonagebusiness.com>
2014-09-16 12:37:15 -0400

    Merge conflict resolution

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150216173416) do
+ActiveRecord::Schema.define(:version => 20140101171241) do
 
   create_table "profiles", :force => true do |t|
     t.string   "name"
