5017eb094d6e9be842c59279eabf8a2c01204473
Will Drexler <william.drexler@vonagebusiness.com>
2014-10-10 14:00:25 -0400

    Fix SippParser to grab the last line of the stats file

--- app/services/sipp_parser.rb
+++ app/services/sipp_parser.rb
@@ -38,7 +38,28 @@ class SippParser
   end
 
   def stop
+    populate_data_buffer
+    @data = @data_buffer.slice! /(.+$)/
+    parse_data "#{@headers}#{@data}"
     @running = false
+  end
+
+  def parse_data(data)
+    CSV.parse(data, headers: true, col_sep: ";").each do |row|
+      next unless row
+      data = {
+        time: DateTime.parse(row['CurrentTime']),
+        test_run: @test_run,
+        total_calls: row['TotalCallCreated'],
+        successful_calls: row['SuccessfulCall(P)'],
+        failed_calls: row['FailedCall(P)'],
+        concurrent_calls: row['CurrentCall'],
+        avg_call_duration: row['CallLength(P)'],
+        response_time: row['ResponseTime1(P)'],
+        cps: row['CallRate(P)']
+      }
+      SippData.create data
+    end
   end
 
   def run
@@ -48,21 +69,7 @@ class SippParser
         populate_data_buffer
         load_data
         next unless @data && !@data.empty?
-        CSV.parse("#{@headers}#{@data}", headers: true, col_sep: ";").each do |row|
-          next unless row
-          data = {
-            time: DateTime.parse(row['CurrentTime']),
-            test_run: @test_run,
-            total_calls: row['TotalCallCreated'],
-            successful_calls: row['SuccessfulCall(P)'],
-            failed_calls: row['FailedCall(P)'],
-            concurrent_calls: row['CurrentCall'],
-            avg_call_duration: row['CallLength(P)'],
-            response_time: row['ResponseTime1(P)'],
-            cps: row['CallRate(P)']
-          }
-          SippData.create data
-        end
+        parse_data "#{@headers}#{@data}"
         @data = ''
       rescue EOFError
         sleep 1
